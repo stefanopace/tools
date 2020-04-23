a46cf52557553bff187579fd4f2f541be6c616b1
Stefano Pace <stefanopace01@gmail.com>
2020-04-23 18:19:26 +0200

    implemented new tool: roll

---
+++ roll/printer.py
@@ -0,0 +1,58 @@
+dice = [dice.strip().split('\n') for dice in ['''
+╭───────╮
+│       │
+│   ●   │
+│       │
+╰───────╯
+''','''
+╭───────╮
+│     ● │
+│       │
+│ ●     │
+╰───────╯
+''','''
+╭───────╮
+│     ● │
+│   ●   │
+│ ●     │
+╰───────╯
+''','''
+╭───────╮
+│ ●   ● │
+│       │
+│ ●   ● │
+╰───────╯
+''','''
+╭───────╮
+│ ●   ● │
+│   ●   │
+│ ●   ● │
+╰───────╯
+''','''
+╭───────╮
+│ ●   ● │
+│ ●   ● │
+│ ●   ● │
+╰───────╯
+''']]
+
+def terminal_width():
+	import fcntl, termios, struct
+	th, tw, hp, wp = struct.unpack('HHHH',
+		fcntl.ioctl(0, termios.TIOCGWINSZ,
+		struct.pack('HHHH', 0, 0, 0, 0)))
+	return tw
+
+def print_dices(faces):
+	cols = terminal_width()
+	dice_width = 10
+	dices_in_a_row = cols // dice_width
+	for i in range(0, len(faces), dices_in_a_row):
+		print_row(faces[i: i + dices_in_a_row])
+
+def print_row(faces):
+	for line in range(5):
+		for face in faces:
+			print(dice[face - 1][line], end=' ')
+		print()
+
--- roll/roll.py
+++ roll/roll.py
@@ -1,31 +1,32 @@
 #!/usr/bin/env python3
 
 import argparse
+import random
+from printer import print_dices
 
 parser = argparse.ArgumentParser(description='Dice Roller')
 
 parser.add_argument('-n', '--launches',
                     dest='launches',
                     metavar='[0-9]+',
-                    type=int, nargs=1, default=1,
+                    type=int, nargs=1, default=[1],
                     help='the number of dice to launch (default: 1)')
 
 parser.add_argument('-t', '--threshold',
                     dest='threshold',
                     metavar='[0-9]+',
-                    type=int, nargs=1,
-                    help='if the sum of the dice is lower than this number the program will return a non-zero exit code')
+                    type=int, nargs=1, default=[None],
+                    help='if the sum of the dice is lower than this number the'
+                        +' program will return a non-zero exit code')
 
 parser.add_argument('-s', '--silent',
                     dest='silent',
                     action='store_true',
                     help='does not print the output')
 
-parser.add_argument('-s', '--silent',
-                    dest='silent',
-                    action='store_true',
-                    help='don\'t output anything if there is nothing to do')
-
 args = parser.parse_args()
-
-print(args)
+threshold = args.threshold[0] or 0
+rolled_dices = [random.randint(1,6) for _ in range(args.launches[0])]
+not args.silent and print_dices(rolled_dices)
+total = sum(rolled_dices)
+total < threshold and exit(threshold - total)
