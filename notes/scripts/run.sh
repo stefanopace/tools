2ccf8195248656a864ec5d792c7e7761e149578c
Stefano Pace <stefanopace01@gmail.com>
2020-11-13 09:03:32 +0100

    uscita da notes in caso di nessuna selezione

--- notes/scripts/run.sh
+++ notes/scripts/run.sh
@@ -2,11 +2,16 @@ cd $NOTE_HOME/scripts
 
 if [ "$1" ]; then
 	touch ../notes/"$1"
-	vim ../notes/$1
+	vim ../notes/"$1"
 	exit 0
 fi;
 
 selected="$($NOTE_HOME/scripts/list_notes.sh | fzf --reverse --height 15 --border --no-info)"
+
+if [ -z $selected ]; then
+	exit 0
+fi;
+
 filename="$(echo "${selected}" | sed -E "s/[^\t]*\t(.*)/\1/")"
 
 vim "$NOTE_HOME/notes/$filename"
